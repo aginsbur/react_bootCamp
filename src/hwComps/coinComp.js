966084a8a7d437886cf7093c7b726beeac20934f
Atara Rose <ataraeg613@gmail.com>
2023-06-12 09:33:05 +0300

    pictures work

--- src/hwComps/coinComp.js
+++ src/hwComps/coinComp.js
@@ -6,7 +6,7 @@ export default class CoinComp extends Component {
         let item = this.props.item
         return (
             <div className='col-md-4 border p-2'>
-                <img className='float-left' src={item.img}/>
+                <img className='float-left' src={`https://files.coinmarketcap.com/static/widget/coins_legacy/64x64/${this.props.item.id}.png`}/>
                 <h4>{item.name}</h4>
                 <p>value: {Number(item.price_usd).toFixed(2)}</p>
             </div>
