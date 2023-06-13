ab7e7bd3cb4409f7338d11e40c22254f577c7f39
Atara Rose <ataraeg613@gmail.com>
2023-06-13 10:03:38 +0300

    vip list working

---
+++ src/comps_hw/appHw.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import VipList from './vipList'
+//import AppAtlas from './appAtlas'
+
+
+function AppHw() {
+    return (
+        <React.Fragment>
+            {/* <AppAtlas /> */}
+            <hr />
+            <VipList />
+        </React.Fragment>
+    )
+}
+
+export default AppHw
