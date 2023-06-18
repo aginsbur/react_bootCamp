d3345af479bb75759f770329f5fb41563578c691
Atara Rose <ataraeg613@gmail.com>
2023-06-18 10:41:10 +0300

    first part of class working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import About from "./comps/about";
+import Form from "./comps/form";
+import Home from "./comps/home";
+import AppEmp from "./comps_employee/appEmp";
+import Header from "./comps_static/header";
+//import Counter from "./redux_comps/counter";
+import counterSlice from "./features/counterSlice";
+import todosSlice from "./features/todosSlice";
+
 import './App.css';
+// import AppTodo from "./todoRedux_comps/appTodo";
+
+// הגדרת הסטור הגלובלי שיספוק פרובידר והסלייסים
+// של הרידיוסר שהוא מכיל בתוכו
+const myStore = configureStore({
+  reducer: {
+    counterSlice,
+    todosSlice
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={myStore}>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/employee" element={<AppEmp />} />
+          <Route path="/employee/:company" element={<AppEmp />} />
+          <Route path="/form" element={<Form />} />
+          {/* <Route path="/counter" element={<Counter />} /> */}
+          {/* <Route path="/todos" element={<AppTodo />} /> */}
+          {/* כוכבית נשאיר לעמוד 404 אם לא מוצא אף
+        ראוט יפעיל את מה שעם פאט' של כוכבית */}
+          <Route path="*" element={<h2>Page 404, not found!</h2>} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
